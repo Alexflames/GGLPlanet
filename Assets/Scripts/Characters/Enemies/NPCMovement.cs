c1191f819841e0fee935bcfe18a3f7b7c97cd221
Sadpotato <alexprgrigoriev@gmail.com>
2019-06-07 18:25:17 +0400

    NPC Movement helpful functions, WIP

---
+++ Assets/Scripts/Characters/Enemies/NPCMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class NPCMovement
+{
+    private static readonly float standSpeedMul = 0.0005f;
+
+    public static Vector2 ChaoticMovement(float speed)
+    {
+        var moveDir = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+        moveDir.Normalize();
+        return moveDir * (speed * standSpeedMul);
+    }
+
+    public static Vector2 ChaoticMovementAdditive(Vector2 oldMove, float speed)
+    {
+        var moveDir = oldMove + new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+        moveDir.Normalize();
+        return moveDir * (speed * standSpeedMul);
+    }
+
+    public static Vector2 DirectionalMovement(Vector2 dir, float speed)
+    {
+        return dir * (speed * standSpeedMul);
+    }
+
+    public static Vector2 DirectionalMovement(Vector2 from, Vector2 to, float speed)
+    {
+        return (to - from) * (speed * standSpeedMul);
+    }
+}
