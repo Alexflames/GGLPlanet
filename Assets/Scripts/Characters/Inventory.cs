a89ade091b1ebd8221b9602761ef0e230cb46f60
MSNUltimatum <35264174+MSNUltimatum@users.noreply.github.com>
2019-06-08 19:28:26 +0400

    Inventory
    Добавлен начальный инвентарь, который открывается на I, а так же базовый стрипт для Items

---
+++ Assets/Scripts/Characters/Inventory.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private List<ItemBase> items;
+
+    public GameObject cellContainer;
+
+    private KeyCode showInventory = KeyCode.I;
+    private void Start()
+    {
+        cellContainer.SetActive(false);
+        items = new List<ItemBase>();
+        for(int i = 0;i < cellContainer.transform.childCount ;i++)
+        {
+            items.Add(new ItemBase());
+        }
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(showInventory))
+        {
+            if(cellContainer.activeSelf)
+            {
+                cellContainer.SetActive(false);
+                Time.timeScale = 1f;
+            }
+            else
+            {
+                cellContainer.SetActive(true);
+                Time.timeScale = 0f;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Characters/ItemBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBase : MonoBehaviour
+{
+    public string NameOfItem;
+
+    public int id;
+
+    public int countItem;
+
+    public bool IsStackable;
+
+    [Multiline(4)]
+    public string DescriptionItem;
+
+    public string pathIcon;
+
+    public string pathPrefab;
+}
