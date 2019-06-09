4f16a540c62181e04241498db0fb389d2eb86eb7
MSNUltimatum <35264174+MSNUltimatum@users.noreply.github.com>
2019-06-09 18:51:14 +0400

    Inventor 1.3

--- Assets/Scripts/Characters/Player/Inventory.cs
+++ Assets/Scripts/Characters/Player/Inventory.cs
@@ -6,29 +6,32 @@ public class Inventory : MonoBehaviour
 {
     private List<ItemBase> items;
 
-    public GameObject cellContainer;
+    public GameObject cellContainerPrefab;
 
     private KeyCode showInventory = KeyCode.I;
 
     private GameObject player;
+
+    private GameObject cellContainer;
+
+    private GameObject Canvas;
+
     private void Start()
     {
-        cellContainer.SetActive(false);
+        Canvas = GameObject.Find("Canvas");
+        cellContainer = Instantiate(cellContainerPrefab, Canvas.transform.position, Quaternion.identity) as GameObject;
+        cellContainer.transform.SetParent(Canvas.transform);
+
         items = new List<ItemBase>();
         for(int i = 0;i < cellContainer.transform.childCount ;i++)
         {
             items.Add(new ItemBase());
         }
+        cellContainer.SetActive(false);
     }
 
     private void Update()
     {
-        if (!player)
-        {
-            player = GameObject.FindWithTag("Player");
-        }
-        else
-        {
             if (Input.GetKeyDown(showInventory))
             {
                 if (cellContainer.activeSelf)
@@ -40,7 +43,6 @@ public class Inventory : MonoBehaviour
                     cellContainer.SetActive(true);
                 }
             }
-        }
     }
 
 }
