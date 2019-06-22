e86692d81cf4f8c1284bec168aadd374dd4a4b4f
Sadpotato <alexprgrigoriev@gmail.com>
2019-06-22 12:58:04 +0400

    merged from develop

---
+++ Assets/Scripts/Characters/Player/CollectableItems.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableItems : MonoBehaviour
+{
+    private Inventory Inventory1;
+
+    private List<ItemBase> ItemBases = new List<ItemBase>();
+
+    private void Start()
+    {
+        Inventory1 = GetComponentInParent<Inventory>();
+    }
+
+   private void OnTriggerEnter2D(Collider2D collider)
+    {
+        ItemBase item = collider.GetComponent<ItemBase>();
+        if(item)
+        {
+            ItemBases.Add(item);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider)
+        {
+            ItemBase item = collider.GetComponent<ItemBase>();
+            if (item)
+            {
+                for (int i = 0; i < ItemBases.Count; i++)
+                {
+                    if (ItemBases[i] == item)
+                    {
+                        ItemBases.RemoveAt(i);
+                    }
+                }
+            }
+        }
+    }
+
+    public void Control()
+    {
+        if (ItemBases.Count > 0)
+        {
+            ItemBase item = ItemBases[0];
+            Inventory1.CollectItems(item);
+            Destroy(ItemBases[0].gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Characters/Player/Inventory.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    private List<ItemBase> items;
+
+    private KeyCode showInventory = KeyCode.I;
+    private KeyCode collectItems = KeyCode.F;
+
+    private GameObject cellContainer;
+    private GameObject Canvas;
+    public GameObject cellContainerPrefab;
+
+    private CollectableItems collectableItems;
+
+    private void Start()
+    {
+        collectableItems = GetComponentInChildren<CollectableItems>();
+
+        Canvas = GameObject.Find("Canvas");
+        cellContainer = Instantiate(cellContainerPrefab, Canvas.transform.position, Quaternion.identity) as GameObject;
+        cellContainer.transform.SetParent(Canvas.transform);
+
+        items = new List<ItemBase>();
+        for (int i = 0; i < cellContainer.transform.childCount; i++)
+        { 
+            items.Add(cellContainer.transform.GetChild(i).GetComponent<ItemBase>());
+        }
+        cellContainer.SetActive(false);
+    }
+
+    private void Update()
+    {
+        KeyDown();
+    }
+
+    private void KeyDown()
+    {
+        if (Input.GetKeyDown(showInventory))
+        {
+            if (cellContainer.activeSelf)
+            {
+                cellContainer.SetActive(false);
+            }
+            else
+            {
+                cellContainer.SetActive(true);
+            }
+        }
+        if(Input.GetKeyDown(collectItems))
+        {
+            collectableItems.Control();
+        }
+    }
+
+    public void CollectItems(ItemBase newItem)
+    { 
+        if (newItem != null)
+        {
+            for (int i = 0;i < items.Count;i++)
+            {
+                if (items[i].id == 0)
+                {
+                    items[i] = newItem;
+                    DisplayItems();
+                    newItem = null;
+                    break;
+                }
+            }
+        }
+    }
+
+    private void DisplayItems()
+    {
+        for(int i = 0;i < items.Count;i++)
+        {
+            Transform cell = cellContainer.transform.GetChild(i);
+            Transform icon = cell.GetChild(0);
+            Image img = icon.GetComponent<Image>();
+            if (items[i].id != 0)
+            {
+                img.sprite = Resources.Load<Sprite>(items[i].pathIcon);
+                img.enabled = true;
+            } 
+            else
+            {
+                img.enabled = false;
+                img.sprite = null;
+            }
+        }
+    }
+        
+}
