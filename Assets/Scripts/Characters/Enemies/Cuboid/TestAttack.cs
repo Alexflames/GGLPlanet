e7a5769d7be7aab5caaa21e9a0316418a49d7762
Sadpotato <alexprgrigoriev@gmail.com>
2019-07-01 15:34:20 +0400

    New attack: does nothing

---
+++ Assets/Scripts/Characters/Enemies/Cuboid/TestAttack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAttack : CuboidAttack
+{
+    public override int priority
+    {
+        get
+        {
+            return 10;
+        }
+    }
+
+    [SerializeField]
+    private float AttackDuration;
+    public override float duration
+    {
+        get
+        {
+            return AttackDuration;
+        }
+    }
+
+    [SerializeField]
+    private Color AttackColor;
+    public override Color attackColor
+    {
+        get
+        {
+            return AttackColor;
+        }
+    }
+
+    public override void AttStart()
+    {
+        print("Attack started!");
+    }
+
+    public override void AttUpdate(float attackTimeLeft)
+    {
+        print("Attack updated!");
+    }
+
+    public override void AttEnd()
+    {
+        print("Attack ended!");
+    }
+}
