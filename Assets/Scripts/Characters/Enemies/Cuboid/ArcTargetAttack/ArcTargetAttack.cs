b6a4abc3d48c8186fe214aa40f8ab9444f5f1916
Nikita Vasin(notebook) <nikitavsn64rus@gmail.com>
2019-07-04 13:34:43 +0400

    New attack: Radiance

---
+++ Assets/Scripts/Characters/Enemies/Cuboid/ArcTargetAttack/ArcTargetAttack.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcTargetAttack : CuboidAttack
+{
+    public override int priority
+    {
+        get
+        {
+            return 1;
+        }
+    }
+
+    [SerializeField]
+    private float AttackDuration;
+    public override float duration
+    {
+        get
+        {
+            return AttackDuration;
+        }
+    }
+
+    [SerializeField]
+    private Color AttackColor;
+    public override Color attackColor
+    {
+        get
+        {
+            return AttackColor;
+        }
+    }
+
+    [SerializeField]
+    private float SphereSpeed = 2;
+
+    [SerializeField]
+    private GameObject arcSphere;
+    private GameObject player; // Player in scene
+
+    [SerializeField]
+    private float ArcAngle = 45;
+    [SerializeField]
+    private int SphereNumber = 3;
+
+    public void CreateSphere(Vector3 dirToPlayer)
+    {
+        var arcSphereInstance = GameObject.Instantiate(arcSphere,
+            gameObject.transform.position + dirToPlayer,
+            Quaternion.identity);
+
+        var arcSphereLogic = arcSphereInstance.GetComponent<ArcSphereLogic>();
+        arcSphereLogic.SetDirection(dirToPlayer);
+        arcSphereLogic.SetSpeed(SphereSpeed);
+
+        Destroy(arcSphereInstance, 2);
+    }
+
+    public override void AttStart()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player == null) return;
+
+        var dirToPlayer = Vector3.Normalize(player.transform.position - gameObject.transform.position);
+
+        for (int i = 0; i < SphereNumber; i++)
+        {
+            var angle = Mathf.Lerp(-ArcAngle / 2, ArcAngle / 2, i / (SphereNumber - 1.0f));
+            var rotation = Quaternion.Euler(0, 0, angle);
+            var newDirToPlayer = rotation * dirToPlayer;
+            CreateSphere(newDirToPlayer);
+        }
+
+    }
+
+    public override void AttUpdate(float attackTimeLeft)
+    {
+        if (player == null) return;
+
+        print("Attack updated!");
+    }
+
+    public override void AttEnd()
+    {
+        if (player == null) return;
+
+        print("Attack ended!");
+    }
+}
---
+++ Assets/Scripts/Characters/Enemies/Cuboid/Radiance/ZoneLogic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneLogic : MonoBehaviour
+{
+
+    private Vector2 direction = Vector2.zero;
+
+    public void SetDirection(Vector2 dir)
+    {
+        direction = dir;
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (gameObject == null) return;
+
+        if (coll.name.ToLower() == "heart")
+        {
+            StatsManager stats = coll.transform.parent.gameObject.GetComponent<StatsManager>();
+            AttackInformation attack = new AttackInformation(gameObject, 3);
+            stats.DealDamage(attack);
+            Destroy(gameObject);
+        }
+    }
+
+}
