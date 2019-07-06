1ae32a71adfd791a5155c1d91510e942b40d8a0f
Nikita Vasin(notebook) <nikitavsn64rus@gmail.com>
2019-07-06 13:27:02 +0400

    New attack Wisp(mb need to fix)

---
+++ Assets/Scripts/Characters/Enemies/Cuboid/Wisp/WispAttack.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WispAttack : CuboidAttack
+{
+
+    [SerializeField]
+    private float attackDuration;
+
+    public override float duration
+    {
+        get
+        {
+            return attackDuration;
+        }
+    }
+
+    public override int priority
+    {
+        get
+        {
+            return 1;
+        }
+    }
+
+    [SerializeField]
+    private Color attColor;
+
+    public override Color attackColor
+    {
+        get
+        {
+            return attColor;
+        }
+    }
+
+    [SerializeField]
+    private GameObject wisp;
+    private GameObject player;
+
+    
+
+    [SerializeField]
+    private int WispNumber = 30;
+
+    [SerializeField]
+    private float WispSpeed = 1.6f;
+
+    public void CreateWisp(Vector3 dirOfCube)
+    {
+        var wispInstance = GameObject.Instantiate(wisp,
+            dirOfCube,
+            Quaternion.Euler(0, 0, Random.Range(0, 360)));
+        var wispLogic = wispInstance.GetComponent<WispLogic>();
+        wispLogic.SetDirection(dirOfCube);
+        wispLogic.SetSpeed(WispSpeed);
+        Destroy(wispInstance, 2);
+    }
+    public override void AttStart()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null) return;
+        var dirOfCube = gameObject.transform.position;
+        for (int i = 0; i < WispNumber; i++)
+        {
+            CreateWisp(dirOfCube);
+        }
+       
+    }
+
+    public override void AttUpdate(float attackTimeLeft)
+    {
+        if (player == null) return;
+        print("Attack updated: time left " + attackTimeLeft);
+    }
+
+    public override void AttEnd()
+    {
+        if (player == null) return;
+        print("Attack ended");
+    }
+
+}
---
+++ Assets/Scripts/Characters/Enemies/Cuboid/Wisp/WispLogic.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WispLogic : MonoBehaviour
+{
+
+    private Vector2 direction = Vector2.zero;
+    private float Speed = 1;
+
+    public void SetSpeed(float speedToSet)
+    {
+        Speed = speedToSet;
+    }
+    public void SetDirection(Vector2 dir)
+    {
+        direction = dir;
+    }
+    void Update()
+    {
+        transform.Translate(direction * Time.deltaTime * Speed);
+    }
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (gameObject == null) return;
+
+        if (coll.name.ToLower() == "heart")
+        {
+            StatsManager stats = coll.transform.parent.gameObject.GetComponent<StatsManager>();
+            AttackInformation attack = new AttackInformation(gameObject, 1);
+            stats.DealDamage(attack);
+            Destroy(gameObject);
+        }
+        else if (coll.gameObject.tag == "Environment")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
